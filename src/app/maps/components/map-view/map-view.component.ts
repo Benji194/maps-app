fc43d29ba411c63db315bbbaae5e21ed9369d350
marturo <vazben94@gmail.com>
2024-03-03 13:19:57 -0600

    butons and location

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { PlacesService } from '../../services';
-import { Map } from 'mapbox-gl';
+import { Map , Popup ,Marker } from 'mapbox-gl';
 
 @Component({
   selector: 'app-map-view',
@@ -25,7 +25,19 @@ export class MapViewComponent implements AfterViewInit {
       center: this.placesService.userLocation, // starting position [lng, lat]
       zoom: 14, // starting zoom
     });
+
+    const popup  = new Popup ().setHTML(`
+      <h6> Aqui estoy </h6>
+      <span>Estoy en este lugar del mundo </span>
+
+    `);
+
+    new Marker({color: 'red'}).setLngLat(this.placesService.userLocation)
+    .setPopup(popup)
+    .addTo(map);
+
   }
+
 
 
 
