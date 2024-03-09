8363539f5da3b8602568d2cb9f535381ee23d6eb
marturo <vazben94@gmail.com>
2024-03-08 20:39:06 -0600

    search with api

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Feature, PlacesResponce } from '../interfaces/places';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -6,12 +9,16 @@ import { Injectable } from '@angular/core';
 export class PlacesService {
 
   public userLocation ?: [number ,  number]  ;
+  public isLoadingPlaces : boolean = false ;
+  public places : Feature[] = [] ;
 
   get isUserLocationReady ( ): boolean {
     return  !!this.userLocation
   }
 
-  constructor() {
+  constructor(
+    private http : HttpClient
+  ) {
     this.getUserLocation();
   }
 
@@ -34,6 +41,24 @@ export class PlacesService {
 
 
       }  );
+  }
+
+
+  getPlacesByQuery( query : string = '' ){
+    // todo evaluar cuand oel query es un nulo
+
+    this.isLoadingPlaces =true ;
+
+    this.http.get<PlacesResponce>(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?proximity=-73.990593%2C40.740121&language=es&access_token=${ environment.apiKey }`)
+    .subscribe(
+      res => {
+        console.log(res.features);
+
+        this.isLoadingPlaces =false ;
+        this.places = res.features
+
+      }
+    );
   }
 
 }
